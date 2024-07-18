eaba33b423a324e018c7d68a890bc2ab5584f64e
Conanvs1022 <79007417+Conanvs1022@users.noreply.github.com>
2024-07-18 13:52:03 +0900

    Update evflownet.py

--- src/models/evflownet.py
+++ src/models/evflownet.py
@@ -2,36 +2,63 @@ import torch
 from torch import nn
 from src.models.base import *
 from typing import Dict, Any
+import gc
 
 _BASE_CHANNELS = 64
 
 class EVFlowNet(nn.Module):
     def __init__(self, args):
-        super(EVFlowNet,self).__init__()
+        super(EVFlowNet, self).__init__()
         self._args = args
+        self.dropout_rate = 0.1
 
-        self.encoder1 = general_conv2d(in_channels = 4, out_channels=_BASE_CHANNELS, do_batch_norm=not self._args.no_batch_norm)
-        self.encoder2 = general_conv2d(in_channels = _BASE_CHANNELS, out_channels=2*_BASE_CHANNELS, do_batch_norm=not self._args.no_batch_norm)
-        self.encoder3 = general_conv2d(in_channels = 2*_BASE_CHANNELS, out_channels=4*_BASE_CHANNELS, do_batch_norm=not self._args.no_batch_norm)
-        self.encoder4 = general_conv2d(in_channels = 4*_BASE_CHANNELS, out_channels=8*_BASE_CHANNELS, do_batch_norm=not self._args.no_batch_norm)
 
-        self.resnet_block = nn.Sequential(*[build_resnet_block(8*_BASE_CHANNELS, do_batch_norm=not self._args.no_batch_norm) for i in range(2)])
+        self.encoder1 = general_conv2d(in_channels = 4, 
+                                       out_channels=_BASE_CHANNELS, 
+                                       do_batch_norm=not self._args.no_batch_norm,
+                                       dropout_prob=0.1)
+        self.encoder2 = general_conv2d(in_channels = _BASE_CHANNELS, 
+                                       out_channels=2*_BASE_CHANNELS, 
+                                       do_batch_norm=not self._args.no_batch_norm,
+                                       dropout_prob=0.1)
+        self.encoder3 = general_conv2d(in_channels = 2*_BASE_CHANNELS, 
+                                       out_channels=4*_BASE_CHANNELS, 
+                                       do_batch_norm=not self._args.no_batch_norm,
+                                       dropout_prob=0.1)
+        self.encoder4 = general_conv2d(in_channels = 4*_BASE_CHANNELS, 
+                                       out_channels=8*_BASE_CHANNELS, 
+                                       do_batch_norm=not self._args.no_batch_norm,
+                                       dropout_prob=0.1)
+        gc.collect()
+        torch.cuda.empty_cache()
+
+
+        self.resnet_block = nn.Sequential(
+            *[build_resnet_block(8*_BASE_CHANNELS, do_batch_norm= not self._args.no_batch_norm, dropout_rate=0.1) for i in range(2)]
+        )
 
         self.decoder1 = upsample_conv2d_and_predict_flow(in_channels=16*_BASE_CHANNELS,
-                        out_channels=4*_BASE_CHANNELS, do_batch_norm=not self._args.no_batch_norm)
-
+                                                         out_channels=4*_BASE_CHANNELS, 
+                                                         do_batch_norm=not self._args.no_batch_norm,
+                                                         dropout_prob=0.1)
         self.decoder2 = upsample_conv2d_and_predict_flow(in_channels=8*_BASE_CHANNELS+2,
-                        out_channels=2*_BASE_CHANNELS, do_batch_norm=not self._args.no_batch_norm)
-
+                                                         out_channels=2*_BASE_CHANNELS, 
+                                                         do_batch_norm=not self._args.no_batch_norm,
+                                                         dropout_prob=0.1)
         self.decoder3 = upsample_conv2d_and_predict_flow(in_channels=4*_BASE_CHANNELS+2,
-                        out_channels=_BASE_CHANNELS, do_batch_norm=not self._args.no_batch_norm)
-
+                                                         out_channels=_BASE_CHANNELS, 
+                                                         do_batch_norm=not self._args.no_batch_norm,
+                                                         dropout_prob=0.1)
         self.decoder4 = upsample_conv2d_and_predict_flow(in_channels=2*_BASE_CHANNELS+2,
-                        out_channels=int(_BASE_CHANNELS/2), do_batch_norm=not self._args.no_batch_norm)
+                                                         out_channels=int(_BASE_CHANNELS/2), 
+                                                         do_batch_norm= not self._args.no_batch_norm,
+                                                         dropout_prob=0.1)
+        # gc.collect()
+        # torch.cuda.empty_cache()
 
     def forward(self, inputs: Dict[str, Any]) -> Dict[str, Any]:
-        # encoder
         skip_connections = {}
+
         inputs = self.encoder1(inputs)
         skip_connections['skip0'] = inputs.clone()
         inputs = self.encoder2(inputs)
@@ -41,28 +68,32 @@ class EVFlowNet(nn.Module):
         inputs = self.encoder4(inputs)
         skip_connections['skip3'] = inputs.clone()
 
-        # transition
         inputs = self.resnet_block(inputs)
 
-        # decoder
         flow_dict = {}
         inputs = torch.cat([inputs, skip_connections['skip3']], dim=1)
         inputs, flow = self.decoder1(inputs)
         flow_dict['flow0'] = flow.clone()
+        inputs = F.dropout(inputs, p=self.dropout_rate, training=self.training)
 
         inputs = torch.cat([inputs, skip_connections['skip2']], dim=1)
         inputs, flow = self.decoder2(inputs)
         flow_dict['flow1'] = flow.clone()
+        inputs = F.dropout(inputs, p=self.dropout_rate, training=self.training)
 
         inputs = torch.cat([inputs, skip_connections['skip1']], dim=1)
         inputs, flow = self.decoder3(inputs)
         flow_dict['flow2'] = flow.clone()
+        inputs = F.dropout(inputs, p=self.dropout_rate, training=self.training)
 
         inputs = torch.cat([inputs, skip_connections['skip0']], dim=1)
         inputs, flow = self.decoder4(inputs)
         flow_dict['flow3'] = flow.clone()
+        inputs = F.dropout(inputs, p=self.dropout_rate, training=self.training)
+        gc.collect()
+        torch.cuda.empty_cache()
 
-        return flow
+        return skip_connections, flow_dict
         
 
 # if __name__ == "__main__":
